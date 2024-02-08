ae4fa22a7bff5bb7fcbdbd2d2d7a64db89787985
Elizabeth <equispee@proveedor.intercorp.com.pe>
2024-02-08 09:50:27 -0500

    Initial Commit

---
+++ HelloiOS/HelloiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloiOSApp.swift
+//  HelloiOS
+//
+//  Created by Elizabeth Quispe on 8/02/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
